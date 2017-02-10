bc7d8d5b0728c91c58871a1c9fe25ffb18171e99
Jesse Aldridge <JesseAldridge@gmail.com>
2017-02-10 15:46:19 -0800

    first

---
+++ parse_csv.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+import sys, codecs, csv, json
+
+
+def parse_csv(filename):
+  # Read the passed csv file into a list of objects.
+
+  with codecs.open(filename, encoding='utf-8') as f:
+    reader = csv.reader(utf_8_encoder(f), delimiter=',', quotechar='"', skipinitialspace=True)
+    rows = [row for row in reader]
+  dicts = []
+  if rows:
+    keys = rows[0]
+    keys = [key.strip().lower().replace(' ', '_') for key in keys]
+    d = {}
+    dicts.append(d)
+    for row in rows[1:]:
+      for key, val in zip(keys, row):
+        d[key] = val.decode('utf8')
+  return dicts
+
+def utf_8_encoder(f):
+    for line in f:
+      yield line.encode('utf-8')
+
+if __name__ == '__main__':
+  print json.dumps(parse_csv(sys.argv[1]), indent=2)
