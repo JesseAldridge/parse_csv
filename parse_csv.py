e1fed01c32a73ffb50f6c0f80baac806b35caba8
Jesse Aldridge <jessealdridge@gmail.com>
2018-11-28 00:26:29 -0800

    handle iso-8859-1

--- parse_csv.py
+++ parse_csv.py
@@ -5,9 +5,13 @@ import sys, codecs, csv, json
 def parse_csv(filename):
   # Read the passed csv file into a list of objects.
 
-  with codecs.open(filename, encoding='utf-8') as f:
-    reader = csv.reader(utf_8_encoder(f), delimiter=',', quotechar='"', skipinitialspace=True)
-    rows = [row for row in reader]
+  for encoding in 'utf-8', "ISO-8859-1":
+    try:
+      with open(filename, encoding=encoding) as f:
+        reader = csv.reader(f, delimiter=',', quotechar='"', skipinitialspace=True)
+        rows = [row for row in reader]
+    except UnicodeDecodeError:
+      pass
   dicts = []
   if rows:
     keys = rows[0]
@@ -16,14 +20,9 @@ def parse_csv(filename):
       d = {}
       dicts.append(d)
       for key, val in zip(keys, row):
-        d[key] = val.decode('utf8')
+        d[key] = val
   return dicts
 
-def utf_8_encoder(f):
-    for line in f:
-      yield line.encode('utf-8')
-
-if __name__ == '__main__':
-  path = '/Users/Jesse/Desktop/Daily Log - Sheet1.csv'
-  print json.dumps(parse_csv(path), indent=2)
-  # print json.dumps(parse_csv(sys.argv[1]), indent=2)
+path = sys.argv[1]
+rows = parse_csv(path)
+print(rows)
